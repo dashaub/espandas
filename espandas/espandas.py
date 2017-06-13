c7f7cef8fec1ef514ef96152c32f66178e7b0dc3
David Shaub <davidshaub@gmx.com>
2017-06-12 22:25:57 -0600

    working insert

--- espandas/espandas.py
+++ espandas/espandas.py
@@ -1,5 +1,27 @@
 import pandas as pd
+import ujson as json
 from elasticsearch import Elasticsearch, helpers
 
 es = Elasticsearch()
 
+def generate_dict(df):
+	"""
+	Generator for creating a dict to be inserted into ElasticSearch
+	for each row of a pd.DataFrame
+	:param df: the input pd.DataFrame to use, must contain an '_id' column
+	"""
+	records = df.to_json(orient = 'records')
+	records = json.loads(records)
+	for record in records:
+		yield record
+
+def df_to_es(df, index, _type, es):
+	"""
+	Insert a Pandas DataFrame into ElasticSearch
+	:param df: the DataFrame
+	:param index: the ElasticSearch index
+	:param _type: the ElasticSearch type
+	:param es: the connection to ElasticSearch
+	"""
+	data = ({'_index': index, '_type': _type , '_id': record['_id']} for record in generate_dict(df))
+	helpers.bulk(es, data)
--- tests/test-espandas.py
+++ tests/test-espandas.py
@@ -1,6 +1,31 @@
 import pandas as pd
 import numpy as np
+from elasticsearch import Elasticsearch,helpers
+from elasticsearch.exceptions import RequestError, ConnectionError
 
+# ES variables
+INDEX = 'unit_tests_index'
+TYPE = 'foo_bar'
+
+# Example data frame
 df = pd.DataFrame(np.random.rand(100, 5))
-df.columns = ['featA', 'featB', 'featC', 'featD', 'featE']
+df.columns = ['A', 'B', 'C', 'D', 'E']
+df['_id'] = df['A'] + df['B']
 
+a = json.loads(records)
+
+
+es = Elasticsearch()
+try:
+	es.indices.create(INDEX)
+except RequestError:
+	print 'Index already exists skipping tests'
+	assert True
+except ConnectionError:
+	print  'The ElasticSearch backend is not running. Skipping tests.'
+	assert True
+except Exception as e:
+	print 'An unknown error occured: %s' % e
+
+
+es.indices.delete(INDEX)
