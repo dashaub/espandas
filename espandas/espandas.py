9e8a46ae93cddc850cc8c05a00161e61eaeb138a
David Shaub <davidshaub@gmx.com>
2017-06-13 20:17:38 -0600

    add class

--- espandas/espandas.py
+++ espandas/espandas.py
@@ -2,20 +2,14 @@ import pandas as pd
 import ujson as json
 from elasticsearch import Elasticsearch, helpers
 
-es = Elasticsearch()
+class espandas(object):
+	def __init__(self, **kwargs):
+		self.client = Elasticsearch(**kwargs)
 
-def generate_dict(df):
-	"""
-	Generator for creating a dict to be inserted into ElasticSearch
-	for each row of a pd.DataFrame
-	:param df: the input pd.DataFrame to use, must contain an '_id' column
-	"""
-	records = df.to_json(orient = 'records')
-	records = json.loads(records)
-	for record in records:
-		yield record
+	def es_read(keys):
+	pass
 
-def df_to_es(df, index, _type, es):
+	def es_write(df, index, doc_type):
 	"""
 	Insert a Pandas DataFrame into ElasticSearch
 	:param df: the DataFrame
@@ -23,5 +17,20 @@ def df_to_es(df, index, _type, es):
 	:param _type: the ElasticSearch type
 	:param es: the connection to ElasticSearch
 	"""
-	data = ({'_index': index, '_type': _type , '_id': record['_id']} for record in generate_dict(df))
-	helpers.bulk(es, data)
+
+	def generate_dict(df):
+		"""
+		Generator for creating a dict to be inserted into ElasticSearch
+		for each row of a pd.DataFrame
+		:param df: the input pd.DataFrame to use, must contain an '_id' column
+		"""
+		records = df.to_json(orient = 'records')
+		records = json.loads(records)
+		for record in records:
+			yield record
+
+	# The dataframe should be sorted by column name
+	df = df.reindex_axis(sorted(df.columns), axis = 1)
+
+	data = ({'_index': index, '_type': doc_type , 'eventId': record['eventId'], '_source': record} for record in generate_dict(df))
+	helpers.bulk(self.client, data)
