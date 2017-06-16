65cd8d3beff71db73e73aa321f5b9ac2fefe2ae4
David Shaub <dashaub@users.noreply.github.com>
2017-06-16 08:30:44 -0600

    robustness

--- espandas/espandas.py
+++ espandas/espandas.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import numpy as np
 import ujson as json
 from elasticsearch import Elasticsearch, helpers
 from elasticsearch.exceptions import NotFoundError
 
-class espandas(object):
+class Espandas(object):
 	def __init__(self, **kwargs):
 		"""
 		Construct an espandas reader/writer
@@ -33,20 +34,35 @@ class espandas(object):
 				self.failed_ += 1
 
 		# Prepare the records into a single DataFrame
-		df = pd.concat(records)
-		df.index = [i for i in xrange(df.shape[0])]
-		df.fillna(value = np.nan, inplace = True)
-		df = df.reindex_axis(sorted(df.columns), axis = 1)
+		df= None
+		if len(records) > 1:
+			df = pd.concat(records)
+			df.index = [i for i in xrange(df.shape[0])]
+			df.fillna(value = np.nan, inplace = True)
+			df = df.reindex_axis(sorted(df.columns), axis = 1)
 		return df
 
 
-	def es_write(self, df, index, doc_type):
+	def es_write(self, df, index, doc_type, index_name = 'indexId'):
 		"""
 		Insert a Pandas DataFrame into ElasticSearch
-		:param df: the DataFrame, must contain the column 'eventId' for a unique identifier
+		:param df: the DataFrame, must contain the column 'indexId' for a unique identifier
 		:param index: the ElasticSearch index
 		:param doc_type: the ElasticSearch doc_type
 		"""
+		if not type(df) == pd.core.frame.DataFrame:
+			raise ValueError('df must be a pandas DataFrame')
+
+		if not self.client.indices.exists(index = index):
+			print('index does not exist, creating index')
+			self.client.indices.create(index)
+
+		if not index_name in df.columns:
+			raise ValueError('the index_name must be a column in the DataFrame')
+		
+		if len(df[index_name]) != len(set(df[index_name])):
+			raise ValueError('the values in index_name must be unique to use as an ElasticSearch _id')
+		self.index_name = index_name
 
 		def generate_dict(df):
 			"""
@@ -62,5 +78,5 @@ class espandas(object):
 		# The dataframe should be sorted by column name
 		df = df.reindex_axis(sorted(df.columns), axis = 1)
 
-		data = ({'_index': index, '_type': doc_type , '_id': record['eventId'], '_source': record} for record in generate_dict(df))
+		data = ({'_index': index, '_type': doc_type , '_id': record[index_name], '_source': record} for record in generate_dict(df))
 		helpers.bulk(self.client, data)
