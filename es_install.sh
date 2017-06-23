22837970d7201558becc381407f832e08948ef97
David Shaub <davidshaub@gmx.com>
2017-06-23 07:29:58 -0600

    install script change

--- es_install.sh
+++ es_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ES_VERSION=2.4.5
-wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.4.5/elasticsearch-${ES_VERSION}.tar.gz
+wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${ES_VERSION}/elasticsearch-${ES_VERSION}.tar.gz
 tar xzvf elasticsearch-${ES_VERSION}.tar.gz
 
 ./elasticsearch-${ES_VERSION}/bin/elasticsearch &
