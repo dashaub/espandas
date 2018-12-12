0a1f13c372b86b3c4177817d2789ccd619dc6ad0
David Shaub <dashaub@users.noreply.github.com>
2018-12-12 15:42:10 -0700

    test with ES 2.4.6

--- es_install.sh
+++ es_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ES_VERSION=2.4.5
+ES_VERSION=2.4.6
 wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${ES_VERSION}/elasticsearch-${ES_VERSION}.tar.gz
 tar xzvf elasticsearch-${ES_VERSION}.tar.gz
 
